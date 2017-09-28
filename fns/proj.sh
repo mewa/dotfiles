884caf4ce5a492377b24573531bf5edc0ddf0193
mewa <marcin.k.chmiel@gmail.com>
2017-09-28 04:27:33 +0200

    Added iterative depth proj script variant

--- fns/proj.sh
+++ fns/proj.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
 function proj() {
-    fuzzycd $PROJECTS_HOME $@
+    if [[ $1 == '-i' ]]; then
+	local d=2
+	shift
+	while ! MAXDEPTH=$d fuzzycd $PROJECTS_HOME $@; do
+	    d=$(($d+1))
+	    echo -n \.
+	done
+    else
+	fuzzycd $PROJECTS_HOME $@
+    fi
 }
 
 function fuzzycd() {
     local dir
     local path
     local base
+    local maxdepth
     
     [[ -z $1 ]] && cd $HOME/projects && return 0
+    [[ -n $MAXDEPTH ]] && maxdepth="-maxdepth $MAXDEPTH"
 
     base=$1
     shift
@@ -17,7 +28,7 @@ function fuzzycd() {
 	path=$path$1*
 	shift
     done
-    dir=$(find -L $base -ipath "*$path" -type d | awk -F / '{ printf("%d\t%s\n", NF, $0); }' | sort -n | head -n1 | cut -f2)
+    dir=$(find -L $base -ipath "*$path" -type d $maxdepth | awk -F / '{ printf("%d\t%s\n", NF, $0); }' | sort -n | head -n1 | cut -f2)
 
     [[ -z $dir ]] && return -1
     
