821156c60d30bdd08e78f4ef4339721b20d76c75
mewa <marcin.k.chmiel@gmail.com>
2017-09-30 05:12:43 +0200

    Fixed -maxdepth order in fuzzycd

--- fns/proj.sh
+++ fns/proj.sh
@@ -28,7 +28,7 @@ function fuzzycd() {
 	path=$path$1*
 	shift
     done
-    dir=$(find -L $base -ipath "*$path" -type d $maxdepth | awk -F / '{ printf("%d\t%s\n", NF, $0); }' | sort -n | head -n1 | cut -f2)
+    dir=$(find -L $base $maxdepth -ipath "*$path" -type d | awk -F / '{ printf("%d\t%s\n", NF, $0); }' | sort -n | head -n1 | cut -f2)
 
     [[ -z $dir ]] && return -1
     
